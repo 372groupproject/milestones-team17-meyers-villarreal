828e1847ecce54ed970067ca2d1cba0106efa01e
Graysen Meyers <41168696+gpmeyers@users.noreply.github.com>
2020-03-24 16:52:49 -0700

    First code snippit done

--- code/FirstCodeSnippet/src/ScalaCodeSnippet1.scala
+++ code/FirstCodeSnippet/src/ScalaCodeSnippet1.scala
@@ -6,21 +6,21 @@
 object ScalaCodeSnippet1 {
   def main(args: Array[String]){
     println("Hello World!");
-    
+
     var evenList = for { i <- 1 to 20
-      if(i % 2) == 0
+                         if(i % 2) == 0
     } yield i
-    
+
     val randLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    
+
     for(i <- 0 until randLetters.length){
       println(randLetters(i))
     }
-      
+
     val aList = List(1,2,3,4,5)
     for(i <- aList){
       println("List items " + i)
     }
-    
+
   }
 }
